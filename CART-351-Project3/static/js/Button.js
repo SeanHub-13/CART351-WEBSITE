aadb6e2373496a8535da97f5887bcaed75d9ce8a
SeanHub-13 <coolflame68@gmail.com>
2025-12-15 12:27:48 -0500

    Button.js Comments

--- CART-351-Project3/static/js/Button.js
+++ CART-351-Project3/static/js/Button.js
@@ -3,29 +3,37 @@ class Button extends Phaser.Physics.Arcade.Sprite {
         super(scene, x, y, texture);
 
         this.scene = scene;
+
+        // Stores the function to call when the button is pressed
         this.callback = callback;
+
         this.isPressed = false;
 
         scene.add.existing(this);
         scene.physics.add.existing(this);
 
+        // Pretty important for a fake tile
         this.body.setImmovable(true);
+
         this.body.setAllowGravity(false);
 
+        // Makes the hitbox fit the button perfectly
         this.body.setSize(this.width * 0.7, this.height * 0.7);
     }
 
+    // If pressed by a player
     press(player) {
+        // Stops the button from being pressed many times
         if (this.isPressed) return;
 
         this.isPressed = true;
 
+        // Makes it darker
         this.setTint(0xaaaaaa);
 
+        // If a callback function was provided, call it with context
         if (this.callback) {
             this.callback(player, this);
         }
-
     }
-
 }
